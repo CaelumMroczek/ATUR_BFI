a3853641265da806a0633c2eb237c4316ee58671
Caelum Mroczek <cmroczek@nau.edu>
2024-07-16 15:56:56 -0700

    edited BFI predict fx

--- code/BFI-predict-function.R
+++ code/BFI-predict-function.R
@@ -24,13 +24,13 @@ BFI.predictor <- function(input_dataframe, model_path) {
   
   # Load required datasets
   
-  # Load HUC8 basin raster
-  HUC8_Basins <- terra::rast(here("data/variables/AZ_DEM_30M_latlong.tif"))
-  HUC8_Basins <- project(HUC8_Basins, "+proj=longlat +datum=WGS84")
+  # Load GWBasin shapefile
+  GW_Basins <- terra::vect(here("data/GW_shapefile"))
+  GW_Basins <- project(GW_Basins, "+proj=longlat +datum=WGS84")
   
   # Load DEM raster
   ###RASTER IS TOO LARGE, USE 30M RASTER OF AZ
-  #DEM <- rast(here("data/variables/huc8.tif"))
+  DEM <- terra::rast(here("data/variables/AZ_DEM_30M_latlong.tif"))
 
   #Load precip csv
   precip_df <- read.csv(here("data/variables/GW_Precipitation_ANNUAL.csv"))
@@ -41,30 +41,30 @@ BFI.predictor <- function(input_dataframe, model_path) {
   #Load ET data
   et_df <- read.csv(here("data/variables/GW_ET_ANNUAL.csv"))
 
-  # Load HUC predictors
-  HUC_Predictors <- read_csv(here("data/variables/HUC_Data_FULL.csv"))
+  # Load GWBasin predictors
+  GW_Predictors <- read_csv(here("data/variables/HUC_Data_FULL.csv"))
 
   
   # Initialize progress bar
   pb <- progress_bar$new(total = nrow(River_Points), format = "[:bar] :percent eta: :eta")
   pb$tick(0)
   
-  # Assign HUC and elevation data to points
+  # Assign GWBasin and elevation data to points
   for (i in 1:nrow(River_Points)) {
     p <- vect(River_Points[i, ], geom = c("LONG", "LAT"))
-    huc <- terra::extract(HUC8_Basins, p)
+    GWBasin <- terra::extract(GW_Basins, p)
     elev <- terra::extract(DEM, p)
-    River_Points$HUC8[i] <- as.numeric(as.character(huc[, 2]))
+    River_Points$GWBasin[i] <- as.numeric(as.character(GWBasin[, 2]))
     River_Points$ELEVATION_FT[i] <- (elev[, 2]) * 3.281
     pb$tick()
   }
   
-  print("Added HUC and Elevation to points")
+  print("Added GWBasin and Elevation to points")
   
   # Pre-calculate indices for ppt, temp, and ET data
-  pptHUC <- match(River_Points$HUC8, precip_df$HUC8)
-  tempHUC <- match(River_Points$HUC8, temp_df$HUC8)
-  etHUC <- match(River_Points$HUC8, et_df$HUC8)
+  pptGW <- match(River_Points$GWBasin, precip_df$GWBasin)
+  tempGW <- match(River_Points$GWBasin, temp_df$GWBasin)
+  etGW <- match(River_Points$GWBasin, et_df$GWBasin)
   
   # Initialize progress bar
   pb <- progress_bar$new(total = nrow(River_Points), format = "[:bar] :percent eta: :eta")
@@ -73,29 +73,28 @@ BFI.predictor <- function(input_dataframe, model_path) {
   # Assign yearly data to points
   for (i in 1:nrow(River_Points)) {
     year <- River_Points$YEAR[i]
-    huc <- River_Points$HUC8[i]
+    GWBasin <- River_Points$GWBasin[i]
     whichYear <- paste0("X", year)
     
-    temp <- temp_df[tempHUC[i], whichYear]
+    temp <- temp_df[tempGWBasin[i], whichYear]
     River_Points$TEMP_C[i] <- round(temp, 2)
     
-    ppt <- precip_df[pptHUC[i], whichYear]
+    ppt <- precip_df[pptGWBasin[i], whichYear]
     River_Points$PRECIP_MM[i] <- round(ppt, 2)
     
-    et <- et_df[etHUC[i], whichYear]
+    et <- et_df[etGWBasin[i], whichYear]
     River_Points$ET_MM[i] <- round(et, 2)
     
     pb$tick()
   }
   
   # Merge all data into one dataframe
-  RiverPoints_AllData <- merge(River_Points, HUC_Predictors, by = "HUC8", all.x = TRUE)
-  RiverPoints_AllData <- RiverPoints_AllData[, -which(names(RiverPoints_AllData) == "NAME")]
+  RiverPoints_AllData <- merge(River_Points, GW_Predictors, by = "GWBasin", all.x = TRUE)
   RiverPoints_AllData <- RiverPoints_AllData[, c(1, 3:51)]
   
   # Predict BFI using the XGBoost model
   feature_names <- xgb_model$feature_names
-  RiverPoints_AllData <- RiverPoints_AllData[, c("HUC8", "YEAR", "LAT", "LONG", feature_names)]
+  RiverPoints_AllData <- RiverPoints_AllData[, c("GWBasin", "YEAR", "LAT", "LONG", feature_names)]
   
   RiverPoints_AllData$predictedBFI <- inv.logit(predict(object = xgb_model, newdata = as.matrix(RiverPoints_AllData)[, 5:50]))
   
